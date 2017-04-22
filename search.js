0f3d41dcf56d29f6233a83d75f99f63e48cc48c5
yungshenglu <yungshenglu1994@gmail.com>
2017-04-23 02:54:25 +0800

    Update file path to  in .

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 $('.ajax > .btn-submit').click(function() {
     /* Send value and print */
-    $.get('./ajax.njs', {
+    $.get('ajax.njs', {
         id: $('.ajax-id').val()
     }).done(function(data) {
         $('.ajax-result').html(data);
