df869d1da4e4c2abf196ef6a98caf31836756730
yungshenglu <yungshenglu1994@gmail.com>
2017-04-23 02:47:02 +0800

    Update folder structure.

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 $('.ajax > .btn-submit').click(function() {
     /* Send value and print */
-    $.get('./js/ajax.njs', {
+    $.get('./ajax.njs', {
         id: $('.ajax-id').val()
     }).done(function(data) {
         $('.ajax-result').html(data);
